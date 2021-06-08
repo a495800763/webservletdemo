d4f62b3023df69ec5fd0355d7f9549426d5d074b
liumq <495800763@qq.com>
2021-06-08 23:26:29 +0800

    通过映射实现自定义error页面

---
+++ servlet-01/src/main/java/com/liumq/servlet/ErrorServlet.java
@@ -0,0 +1,24 @@
+package com.liumq.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ErrorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html");
+
+        PrintWriter writer = resp.getWriter();
+        writer.print("<h1>页面找不到错误</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
